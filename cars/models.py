c1b6728d7047a9296088d8a98d6867d35dac0a3b
RAFAŁ <rafal.kowalski555@gmail.com>
2023-01-15 15:45:35 +0100

    typo correction

--- cars/models.py
+++ cars/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Cars (models.Model):
-    brande = models.CharField(max_length= 32)
+    brand = models.CharField(max_length= 32)
     engine_type = models.CharField(max_length= 32)
-    engine_capasity = models.CharField(max_length= 32)
+    engine_capacity = models.CharField(max_length= 32)
     engine_power = models.CharField(max_length= 32)
     is_available = models.BooleanField(default=True)
 
@@ -14,7 +14,7 @@ class Cars (models.Model):
 
 class Customer (models.Model):
     name = models.CharField(max_length= 64)
-    adress = models.CharField(max_length= 32)
+    address = models.CharField(max_length= 32)
     phone_number = models.CharField(max_length= 32)
 
     added = models.DateTimeField(auto_now_add=True)
