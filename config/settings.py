c63481d59ac2556e0d0641f41230168af9d4cdab
Sylwester M <49527586+alexsylvester@users.noreply.github.com>
2023-01-15 14:37:03 +0100

    Add website app with static sites
    - created app 'website'
    - registered in config/settings.py and config/urls.py
    - added html templates: base, head, footer, navbar
    - added html templates: home, contact
    - added views: homepage, contact
    
    modified:   config/settings.py
    modified:   config/urls.py
    new file:   website/__init__.py
    new file:   website/admin.py
    new file:   website/apps.py
    new file:   website/migrations/__init__.py
    new file:   website/models.py
    new file:   website/templates/website/base.html
    new file:   website/templates/website/contact.html
    new file:   website/templates/website/footer.html
    new file:   website/templates/website/head.html
    new file:   website/templates/website/home.html
    new file:   website/templates/website/navbar.html
    new file:   website/tests.py
    new file:   website/urls.py
    new file:   website/views.py

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'website.apps.WebsiteConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
