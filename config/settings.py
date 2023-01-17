26569ba144669f8000d4e9dd2259016ff22b4492
Sylwester M <49527586+alexsylvester@users.noreply.github.com>
2023-01-17 12:02:13 +0100

    Rebase templates with base.html to root

--- config/settings.py
+++ config/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
