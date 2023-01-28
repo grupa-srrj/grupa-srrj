9abd98898073a55178ff857e4e1da5b9b75fb7db
PC <jordi8@o2.pl>
2023-01-28 12:56:51 +0100

    new login and logout. Password reset.

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.views.generic.base import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -25,6 +26,15 @@ urlpatterns = [
     path('', include('website.urls')),
     path('cars/', include('cars.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('password_reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='password/password_reset_done.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name="password/password_reset_confirm.html"),
+         name='password_reset_confirm'),
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='password/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
